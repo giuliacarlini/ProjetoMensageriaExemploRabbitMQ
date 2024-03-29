982f56b0213b4e3d8f7a9bd2cd9ccfd25709b3ce
Giulia Carlini <gcarlini>
2024-03-29 14:04:00 -0300

    Adicionar arquivos de projeto.

---
+++ MensageriaRabbitMQ.Envio/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+Console.WriteLine("Digite sua mensagem e aperte <ENTER>");
+
+while (true)
+{
+    string mensagem = Console.ReadLine();
+
+    if (mensagem == "")
+        break;
+
+    var corpo = Encoding.UTF8.GetBytes(mensagem);
+
+    channel.BasicPublish(exchange: string.Empty,
+        routingKey: "hello",
+        basicProperties: null,
+        body: corpo);
+
+    Console.WriteLine($" [x] Enviado {mensagem}");
+}
---
+++ MensageriaRabbitMQ.Recebimento/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+Console.WriteLine("[*] Aguardando mensagens.");
+
+var consumidor = new EventingBasicConsumer(channel);
+
+consumidor.Received += (model, ea) =>
+{
+    var corpo = ea.Body.ToArray();
+    var mensage = Encoding.UTF8.GetString(corpo);
+
+    Console.WriteLine($"[x] Recebido: {mensage}");
+};
+
+channel.BasicConsume(queue: "hello",
+    autoAck: true,
+    consumer: consumidor);
+
+Console.WriteLine(" Aperte [enter] para sair.");
+Console.ReadLine();
